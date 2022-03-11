b8e2c4937acd3e407d38a75f41c530f1add3be70
Harimad <leesh68481@gmail.com>
2022-03-11 22:15:49 +0900

    Database에 자료 저장하는 법 (한줄이면 끝)

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express')
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 
+var db
 const MongoClient = require('mongodb').MongoClient
 MongoClient.connect(
-  'mongodb+srv://Harimad:tjdwls12@cluster0.cqork.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  'mongodb+srv://Harimad:tjdwls12@cluster0.cqork.mongodb.net/todoapp?retryWrites=true&w=majority',
   function (에러, client) {
     if (에러) return console.log(에러)
-    //서버띄우는 코드는 여기로
+
+    db = client.db('todoapp')
+
+    db.collection('post').insertOne(
+      { 이름: 'John', _id: 100 },
+      function (에러, 결과) {
+        console.log('저장완료')
+      }
+    )
+
     app.listen(8080, function () {
       console.log('listening on 8080')
     })
