88bcac7d6977eed854454baa13547c0db6378546
Harimad <leesh68481@gmail.com>
2022-03-11 20:23:07 +0900

    폼에 입력한 데이터를 서버에 전송하는 법 (POST요청)

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express')
 const app = express()
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(8080, function () {
   console.log('listening on 8080')
 })
 
-app.get('/pet', function (요청, 응답) {
-  응답.send('PET SITE')
+app.get('/', function (요청, 응답) {
+  응답.sendFile(__dirname + '/index.html')
 })
 
-app.get('/write', function (req, res) {
-  res.sendFile(__dirname + '/write.html')
+app.get('/write', function (요청, 응답) {
+  응답.sendFile(__dirname + '/write.html')
 })
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html')
+app.post('/add', function (요청, 응답) {
+  console.log(요청.body)
+  응답.send('전송완료')
 })
